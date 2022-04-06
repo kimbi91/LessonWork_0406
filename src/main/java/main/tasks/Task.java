88c3e4ad89827de85586a3e39f66b7b76d6bd6b5
kimbi91 <subscribefan@gmail.com>
2022-04-06 18:19:23 +0200

    countElemInArray done

--- src/main/java/main/tasks/Task.java
+++ src/main/java/main/tasks/Task.java
@@ -54,7 +54,15 @@ public class Task {
      * Megszámolja, hogy adott elem hányszor szerepel a tömbben.
      */
     public static int countElemInArray(int[] array, int elem) {
-        return -1;
+        int counter = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == elem) {
+                counter++;
+            }
+        }
+
+        return counter;
     }
 
     /**
